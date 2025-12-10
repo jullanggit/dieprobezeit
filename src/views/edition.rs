4c33d60b62f48669bc7aa4eb5879f06901924451
Jul Lang <jullanggit@proton.me>
2025-12-10 11:47:34 +0100

    add white background behind svg's

--- src/views/edition.rs
+++ src/views/edition.rs
@@ -18,7 +18,7 @@ pub fn Edition(id: EditionId) -> Element {
                     h3 { class: "text-2xl", "{data.edition.label()}" }
                     for i in 1..=data.num_pages {
                         div {id: "edition-page-{i}",
-                            style: "display: inline-block;",
+                            style: "display: inline-block; background-color: white;",
                             object {
                                 data: "/svgs/{data.edition.date}/-{i}.svg",
                                 height: "100%",
