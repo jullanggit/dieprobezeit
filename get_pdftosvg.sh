47fecaa96155e739bec4fd3cf0c619aeac21baaa
Jul Lang <jullanggit@proton.me>
2026-01-22 23:10:17 +0100

    remove pdf to svg scripts

--- get_pdftosvg.sh
+++
@@ -1,2 +0,0 @@
-curl -L https://github.com/dmester/pdftosvg.net/releases/download/1.7.1/pdftosvg.exe -o tools/pdftosvg
-chmod +x tools/pdftosvg
--- pdf2svg.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/bash
-
-./tools/pdftosvg pdfs/$1.pdf svgs/$1/
