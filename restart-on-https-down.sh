779fef5d271557873ab38270db4e3ce4cbf849a0
Jul Lang <jullanggit@proton.me>
2026-01-29 18:04:28 +0100

    restart-script: change log location and also print to stdout

--- restart-on-https-down.sh
+++ restart-on-https-down.sh
@@ -4,6 +4,6 @@ URL="https://dieprobezeit.ch"
 curl -f -s -o /dev/null --connect-timeout 10 $URL
 
 if [ $? -ne 0 ]; then
-    echo "HTTPS failed at $(date)" >> /var/log/https-monitor.log
+    echo "HTTPS failed at $(date)" | tee -a ~/https-monitor.log
     docker compose restart pingoo
 fi
