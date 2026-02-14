a584d3e37575adc46428327a5aaee81261bc1838
Jul Lang <jullanggit@proton.me>
2026-02-14 18:22:33 +0100

    cap read time to three minutes per page per day

--- src/track_views.rs
+++ src/track_views.rs
@@ -4,7 +4,8 @@ use crate::{
 };
 use dioxus::prelude::*;
 use sea_orm::{
-    prelude::Expr, ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter, QueryOrder, Set,
+    prelude::Expr, ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter, QueryOrder,
+    QuerySelect, Set,
 };
 use std::str::FromStr;
 use time::{Duration, PrimitiveDateTime, UtcDateTime};
@@ -39,6 +40,9 @@ pub fn get_client_id() -> Option<ClientId> {
     get_cookie(STORAGE_KEY, ClientId::from_str)
 }
 
+/// Record read times from a client.
+/// Merge any reads within 30 minutes.
+/// Cap read time at 3 minutes per page. This resets every day to permit re-reads.
 #[post("/api/record-read-times")]
 pub async fn record_read_times(
     edition_id: EditionId,
@@ -49,6 +53,7 @@ pub async fn record_read_times(
     let db = db();
     let client_id = get_client_id().map_or(NO_ID, |client_id| client_id.0);
 
+    let cap_cutoff = UtcDateTime::now() - Duration::days(1);
     let merge_cutoff = UtcDateTime::now() - Duration::minutes(30);
 
     for (page, time) in page_times
@@ -56,11 +61,34 @@ pub async fn record_read_times(
         .enumerate()
         .filter(|(_, time)| **time != 0.)
     {
-        // try to find an existing entry within the merge cutoff
-        let merge = reads::Entity::find()
+        // all entities with matching client id edition id and page number
+        let matching_entities = reads::Entity::find()
             .filter(reads::Column::ClientId.eq(client_id))
             .filter(reads::Column::EditionId.eq(edition_id))
-            .filter(reads::Column::PageNumber.eq(page as i32))
+            .filter(reads::Column::PageNumber.eq(page as i32));
+
+        // Reads in the last day
+        let read_time_within_day: f32 = matching_entities
+            .clone()
+            .filter(
+                reads::Column::Timestamp
+                    .gt(PrimitiveDateTime::new(cap_cutoff.date(), cap_cutoff.time())),
+            )
+            .select_only()
+            .expr(reads::Column::ReadTime.sum())
+            .into_tuple()
+            .one(db)
+            .await
+            .map_err(|err| ServerFnError::new(err.to_string()))?
+            .unwrap_or(0.0);
+
+        let three_minutes_ms = 3. * 60. * 1000.;
+        if read_time_within_day >= three_minutes_ms {
+            continue;
+        }
+
+        // try to find an existing entry within the merge cutoff
+        let merge_entity = matching_entities
             .filter(reads::Column::Timestamp.gt(PrimitiveDateTime::new(
                 merge_cutoff.date(),
                 merge_cutoff.time(),
@@ -70,7 +98,7 @@ pub async fn record_read_times(
             .await
             .map_err(|err| ServerFnError::new(err.to_string()))?;
 
-        match merge {
+        match merge_entity {
             Some(entity) => {
                 let mut active: reads::ActiveModel = entity.into();
                 let current_read_time = active.read_time.take().unwrap_or(0.0);
